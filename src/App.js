1f28e0800a53cd4c6b5ac978990e865adf1af1f1
D3vss <mohamed_oudoun@um5.ac.ma>
2021-11-01 19:40:06 +0100

    Added info section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Navbar } from "./components";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Navbar />
+      <Route path="/" exact component={Home} />
     </Router>
   );
 }
